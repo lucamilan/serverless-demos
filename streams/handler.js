765de057b8328cf890b3cf203a61cd032ecad627
Luca Milan <luca.milan@gmail.com>
2017-10-27 13:07:14 +0200

    streams

---
+++ streams/handler.js
@@ -0,0 +1,25 @@
+'use strict'
+const AWS = require('aws-sdk')
+const db = new AWS.DynamoDB.DocumentClient()
+
+const write = (payload) => {
+  const params = {
+    TableName: process.env.DB_TABLE,
+    Item: payload
+  }
+  return db.put(params).promise()
+}
+
+module.exports.dynamoAdd = (event, context, callback) => {
+  console.log('event:', JSON.stringify(event), process.env.DB_TABLE)
+  const payload = {
+    id: new Date().getTime().toString(),
+    message: 'Hello WebDev!',
+    event
+  }
+  write(payload).then(result => callback(null, result)).catch(callback)
+}
+
+module.exports.dynamoListener = (event, context, callback) => {
+  console.log('event:', JSON.stringify(event))
+}
